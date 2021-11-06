e21a9282f60e69a4159850a50eeaf93fdc7ecc04
Raja Soundaramourty <rajasoun@icloud.com>
2021-11-06 13:45:59 +0530

    Initial Commit

--- provision/prepare-multipass.sh
+++ provision/prepare-multipass.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 VM_NAME="micro"
+PLAYBOOK="https://raw.githubusercontent.com/rajasoun/traefik-load-balancer/main/provision/docker.yml"
 
 if ! [ -x "$(command -v multipass)" ]; then
     echo 'Error: multipass is not installed.' >&2
@@ -11,5 +12,6 @@ fi
 multipass launch --name $VM_NAME --cpus 2 --mem 2G --disk 5G --cloud-init provision/cloud-init.yaml
 multipass exec $VM_NAME -- sudo apt-get install ansible -y 
 multipass exec $VM_NAME -- ansible-galaxy install geerlingguy.docker
-# multipass exec $VM_NAME -- ansible-playbook /tmp/docker.yml
+multipass exec $VM_NAME -- wget ${PLAYBOOK} -P  /tmp
+multipass exec $VM_NAME -- ansible-playbook /tmp/docker.yml
 multipass shell $VM_NAME
