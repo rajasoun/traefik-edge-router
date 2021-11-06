e7c32b09b33bfea46bd2a65a380196c6e99cf10b
Raja Soundaramourty <rajasoun@icloud.com>
2021-11-06 13:34:49 +0530

    Initial Commit

--- provision/prepare-multipass.sh
+++ provision/prepare-multipass.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 VM_NAME="micro"
-VM_HOME="/home/ubuntu"
 
 if ! [ -x "$(command -v multipass)" ]; then
     echo 'Error: multipass is not installed.' >&2
@@ -12,5 +11,5 @@ fi
 multipass launch --name $VM_NAME --cpus 2 --mem 2G --disk 5G --cloud-init provision/cloud-init.yaml
 multipass exec $VM_NAME -- sudo apt-get install ansible -y 
 multipass exec $VM_NAME -- ansible-galaxy install geerlingguy.docker
-multipass exec $VM_NAME -- ansible-playbook ${VM_HOME}/ansible/docker.yml
+# multipass exec $VM_NAME -- ansible-playbook /tmp/docker.yml
 multipass shell $VM_NAME
