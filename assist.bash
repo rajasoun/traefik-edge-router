4b9890c512e1466745b8af48867f3d74e1904036
Raja Soundaramourty <rajasoun@icloud.com>
2021-11-07 13:10:22 +0530

    web analytics

--- assist.bash
+++ assist.bash
@@ -25,7 +25,7 @@ COMPOSE_FILES=$(construct_compose_files "compose.list")
 echo "docker-compose $COMPOSE_FILES"
 
 SERVICES=(htd-bizapps-monitor)
-API_ENDPOINTS=(dashboard/ metrics health analytics)
+API_ENDPOINTS=(dashboard/ metrics web web/health )
 
 export IP
 export BASE_DIR
