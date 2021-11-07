97bb54d80ee8f9339671763f360f9e7786614a70
Raja Soundaramourty <rajasoun@icloud.com>
2021-11-07 17:12:21 +0530

    web analytics

--- provision/src/lib/web.bash
+++ provision/src/lib/web.bash
@@ -3,19 +3,17 @@
 function display_url_status(){
     local max_secs_run="2"
     HOST="$1"
-    echo $HOST
     # shellcheck disable=SC1083
     HTTP_STATUS="$(curl -s --max-time "${max_secs_run}" -o /dev/null -L -w ''%{http_code}'' "https://${HOST}")"
-    echo $HTTP_STATUS
-    case $choice in
+    case $HTTP_STATUS in
       200)  echo "https://${HOST}  -> Up" ;;
-        *)  echo "https://${HOST}  -> Down" ;;
+      502)  echo "https://${HOST}  -> Down" ;;
     esac
 }
 
 
 function display_app_status(){
-    echo ""
+    echo "Status"
     execute_action "display_url_status"
 }
 
